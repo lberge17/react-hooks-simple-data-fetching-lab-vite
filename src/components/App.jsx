e0b8de4e2b4e97ba60893f7720ffeaf2bd99976e
Laura Berge <lauraeberge@gmail.com>
2025-02-07 14:47:19 -0600

    add solution

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,27 @@
-// create your App component here
+import { useEffect, useState } from 'react';
+
+const App = () => {
+    const [imageUrl, setImageUrl] = useState("")
+    const [loading, setLoading] = useState(true)
+
+    const fetchDog = () => {
+        setLoading(true)
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r => r.json())
+        .then(data => {
+            setImageUrl(data.message)
+            setLoading(false)
+        })
+    }
+
+    useEffect(fetchDog, [])
+
+    return (
+        <div>
+            {loading ? <p>Loading...</p> : <img src={imageUrl} alt="A Random Dog" />}
+            <button aria-label="new dog" onClick={fetchDog}>Change Dog</button>
+        </div>
+    );
+}
+
+export default App;
